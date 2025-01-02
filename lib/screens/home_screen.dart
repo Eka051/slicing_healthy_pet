777a5060fafaa28cce7f78e55b535e8d57a06ccd
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-02 13:07:34 +0700

    feat: Add service selection feature and update home screen layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+var services = [
+  'Vaccine',
+  'Surgery',
+  'SPA & Treatment',
+  'Consultation',
+  'Grooming',
+];
+var selectedService = 0;
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -13,16 +22,54 @@ class HomeScreen extends StatelessWidget {
         body: SafeArea(
             child: Column(
       children: [
-        const SizedBox(height: 22),
+        const SizedBox(height: 20),
         _greetings(),
         const SizedBox(
-          height: 17,
+          height: 16,
         ),
         _card(),
         const SizedBox(
           height: 21,
         ),
         _search(),
+        const SizedBox(
+          height: 20,
+        ),
+        SizedBox(
+          height: 36,
+          child: ListView.separated(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      color: index == selectedService
+                          ? const Color(0xFF818AF9)
+                          : const Color(0xFFF6F6F6),
+                      border: selectedService == index
+                          ? Border.all(
+                              color: const Color(0xFFF1E5E5).withAlpha(56),
+                              width: 2)
+                          : null,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        services[index],
+                        style: GoogleFonts.manrope(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: selectedService == index
+                                ? const Color(0xFFFFFFFF)
+                                : const Color(0xFF3F3E3F).withAlpha(76)),
+                      ),
+                    ),
+                  ),
+              separatorBuilder: (context, index) => const SizedBox(
+                    width: 10,
+                  ),
+              itemCount: services.length),
+        )
       ],
     )));
   }
@@ -38,7 +85,8 @@ class HomeScreen extends StatelessWidget {
         child: TextFormField(
             decoration: InputDecoration(
           border: InputBorder.none,
-          prefixIcon: Icon(FeatherIcons.search),
+          prefixIcon: const Icon(FeatherIcons.search,
+              color: Color(0xFFADACAD), size: 20),
           hintText: 'Find best vaccinate, treatment...',
           hintStyle: GoogleFonts.manrope(
             fontSize: 12,
